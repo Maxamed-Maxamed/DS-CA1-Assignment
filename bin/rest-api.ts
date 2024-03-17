150f15a3201e962409b1952f29eb049861213049
Maxamed Maxamed <20088349@mail.wit.ie>
2024-03-17 22:26:39 +0000

    add sign-in and sign-out

--- bin/rest-api.ts
+++ bin/rest-api.ts
@@ -5,3 +5,4 @@ import { RestAPIStack } from "../lib/rest-api-stack";
 
 const app = new cdk.App();
 new RestAPIStack(app, "RestAPIStack", { env: { region: "eu-west-1" } });
+new RestAPIStack(app, "RestAPIStack2", { env: { region: "eu-west-1" } });
